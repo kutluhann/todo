1c694bf5f81f31a81ea46a7c51d5450d20830b93
kutluhann <kutluhan.sipka@ug.bilkent.edu.tr>
2024-09-04 14:30:24 +0300

    Add revalidate for demo page

--- src/app/demo/page.js
+++ src/app/demo/page.js
@@ -2,6 +2,8 @@ import TodoArea from "@/components/demo/TodoArea";
 import { generateTodoDays } from "@/utils";
 import mockTodos from "./mock-todos";
 
+export const revalidate = 21600
+
 export default async function DemoPage() {
   const days = generateTodoDays()
 
